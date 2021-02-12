ba9b2fe91425e309d7481fbcd51f414d4f56110f
varsh18 <varshiniravi1824@gmail.com>
2021-02-12 23:10:55 +0530

    IMDB Movie review hyperdrive

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from sklearn.linear_model import LogisticRegression
+!pip install contractions
+!pip install nltk
 import argparse
 import os
 import nltk
@@ -16,13 +18,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-!pip install contractions
 import contractions  
-ds_path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds_path="https://raw.githubusercontent.com/Varsh18/IMDB-Moview-Review-Analysis/master/IMDB-Dataset.csv"
+contractions_dict = contractions.contractions_dict
 
-def install_dependancies(){
+def install_dependancies():
     nltk.download('stopwords')
-}
 
 def clean_text(text, remove_stopwords = True):
     '''Remove unwanted characters, stopwords, and format the text to create fewer nulls word embeddings'''
@@ -53,27 +54,30 @@ def clean_text(text, remove_stopwords = True):
     return text
 
 def clean_data(data):
-    contractions_dict = contractions.contractions_dict
+#     install_dependancies()
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
     x_df.sentiment = x_df.sentiment.apply(lambda s: 1 if s == "positive" else 0)
-    reviews = pd.get_dummies(x_df.review , prefix = "review")
-    x_df.drop("reviews", inplace=True, axis=1)
-    x_df = x_df.join(reviews)
+#     reviews = pd.get_dummies(x_df.review , prefix = "review")
+#     x_df.drop("reviews", inplace=True, axis=1)
+#     x_df = x_df.join(reviews)
 
-    x_df['review'] = x_df['review'].apply(lambda x: BeautifulSoup(x, 'lxml').get_text().strip())
     x_df['review'] = x_df['review'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>1]))
     x_df['review'] = x_df['review'].str.replace("[^a-zA-Z]", " ")
-    x_df['review'] = list(map(clean_text, x_df.review_cleaned))
-    
+    x_df['review'] = list(map(clean_text, x_df.review))
+
     combined_reviews = ' '.join(x_df['review'])
     text_series = pd.Series(combined_reviews.split())
     freq_comm = text_series.value_counts()
 
     rare_words = freq_comm[-38000:-1]
     x_df['review'] = x_df['review'].apply(lambda x: ' '.join([word for word in x.split() if word not in rare_words]))
-    y_df = x_df.pop("sentiment")
-    return x_df, y_df
+    X = x_df['review'].values
+    y = x_df['sentiment'].values
+#     y_df = x_df.pop("sentiment")
+
+#    return x_df, y_df
+    return X,y
 
 ds= TabularDatasetFactory.from_delimited_files(path=ds_path)
 
@@ -81,8 +85,15 @@ x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+vectorizer = CountVectorizer()
+vectorizer.fit(x_train)
+
 X_train = vectorizer.transform(x_train)
 X_test = vectorizer.transform(x_test)
+
+print('inside clean_data')
+print('X_train :',X_train)
+print('y_train :',y_train)
 
 run = Run.get_context(allow_offline=True)
 
@@ -105,7 +116,9 @@ def main():
 
     os.makedirs("outputs",exist_ok=True)
     joblib.dump(value=model,filename="./outputs/model.joblib")
+    print('inside main')
+    print('X_train shape',X_train.shape)
+    print('y_train shape',y_train.shape)
 
 if __name__ == '__main__':
     main()
- 
